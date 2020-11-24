543d98d50c042de61c796e19ce8664cc57473aff
XinweiYuan <72956686+XinweiYuan@users.noreply.github.com>
2020-11-24 19:40:14 +0800

    Create Code.cpp

---
+++ 222. 完全二叉树的节点个数/Code.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root)
+            return 0;
+        int count = 1;
+        count += countNodes(root->left);
+        count += countNodes(root->right);
+        return count;
+    }
+};
